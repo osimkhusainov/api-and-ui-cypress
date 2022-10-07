515545e96413b59e385a2de38072a52100da1b64
Osim Khusainov <keedice@gmail.com>
2022-10-07 14:58:54 -0400

    updated login test

--- cypress/integration/loginPage/login.spec.js
+++ cypress/integration/loginPage/login.spec.js
@@ -5,6 +5,8 @@ describe("Login page", () => {
     cy.findByText("Sign up").click();
   });
 
+  after(() => cy.clearFixture("userRequestBody.json"));
+
   it("Create new user with new creds", () => {
     cy.readFixtureFile("userRequestBody.json").then((user) => {
       cy.createUser({ ...user });
